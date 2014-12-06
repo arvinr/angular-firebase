b960e76c21e7a464f534b2eb3ce773d643575271
Arvin Randhawa <arvinrandhawa@gmail.com>
2014-12-06 01:10:03 -0600

    Setup reset button

--- app.js
+++ app.js
@@ -2,4 +2,35 @@ angular.module('scheduleApp', ['firebase'])
 .controller('mainController', function($scope, $firebase){
   var ref = new Firebase("https://shining-heat-9261.firebaseio.com/days");
   var fb = $firebase(ref);
+
+  $scope.reset = function(){
+    fb.$set({
+      monday: {
+        name: 'Monday',
+        slots: {
+          0900: {
+            time: '9:00am',
+            booked: false
+          },
+          0110: {
+            time: '11:00am',
+            booked: false
+          }
+        }
+      },
+      tuesday: {
+        name: 'Teusday',
+        slots: {
+          0900: {
+            time: '9:00am',
+            booked: false
+          },
+          0110: {
+            time: '11:00am',
+            booked: false
+          }
+        }
+      }
+    });
+  }
 });
